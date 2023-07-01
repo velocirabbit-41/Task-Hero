383eae5be1531591d7059b787684af2fa38308ee
Noel Pallivathucal <noelpallivathucal@Noels-MBP.attlocal.net>
2023-07-01 14:49:04 -0700

    testing  get req

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,29 +1,30 @@
 //requiring in database from taskModels file
 //can use db within controllers to query
-const db = require('./Models/taskModels.js');
+const path = require('path');
+const db = require(path.resolve(__dirname, '../models/taskModels.js'));
 //declare an empty obj to call methods on
-const taskController = {};
-
-taskController.getTasks = (req, res, next) => {
-  //define query
-  const queryText = 'select * from tasks';
-  db.query(queryText)
-    .then((data) => {
-      res.locals = data;
-      return next();
-    })
-    .catch((err) => {
-      const mwError = {
-        log: 'error in the taskController.getTasks middleware function',
-        status: 400,
-        message: { error: 'was not able to acquire task' },
-      };
-      return next(mwError);
-    });
+const taskController = {
+  getTasks: (req, res, next) => {
+    //define query
+    const queryText = 'select * from tasks';
+    db.query(queryText)
+      .then((data) => {
+        res.locals = data;
+        return next();
+      })
+      .catch((err) => {
+        const mwError = {
+          log: 'error in the taskController.getTasks middleware function',
+          status: 400,
+          message: { error: 'was not able to acquire task' },
+        };
+        return next(mwError);
+      });
+  },
 };
 
-taskController.createTask = (req, res, next) => {};
+// taskController.createTask = (req, res, next) => {};
 
-taskController.assignTask;
+// taskController.assignTask;
 
-taskController.deleteTask;
+// taskController.deleteTask;
