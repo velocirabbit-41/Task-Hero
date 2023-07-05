f0794a101c9c48ee0bbfccbe1b2eb64cd293691a
Josh Hall <joshuarhall0@gmail.com>
2023-07-05 10:16:36 -0700

    cleaning up and simplifying React comps
    Co-authored-by: CameronKirksey21 <CameronKirksey21@users.noreply.github.com>

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+// right now this is a mess, copied from App.jsx
+
+import React, {useEffect, useState} from "react";
+
+const Header = () => {
+
+  // will depend on how we get to login page. 
+  // If we do conditional rendering
+  const [wasClicked, setWasClicked] = useState(false);
+  // const getPageClicker = () => {
+  //   setWasClicked(true);
+    
+  // }
+  // If we do a simple <a href... /> then it just worked like a link
+
+  return (
+    <div>
+      {/* <button onClick={getPageClicker}>login</button> */}
+      <button onclick = "window.location.href='www.linktothepage.com';">
+        login
+      </button>
+    </div>
+  )
+}
+
+export default Header;
+
+
+
+
